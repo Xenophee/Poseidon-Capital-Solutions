84b4d9e859e343ee1e76f0438c93528730390842
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-11-08 12:55:33 +0100

    Ajout de la regex de mdp

--- src/main/java/com/nnk/springboot/domain/UserEntity.java
+++ src/main/java/com/nnk/springboot/domain/UserEntity.java
@@ -2,6 +2,7 @@ package com.nnk.springboot.domain;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,6 +22,10 @@ public class UserEntity {
     private String username;
 
     @NotBlank(message = "Password is mandatory")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$!?%^&+=]).{8,}$",
+            message = "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial (@#$!?%^&+=)"
+    )
     private String password;
 
     @NotBlank(message = "FullName is mandatory")
