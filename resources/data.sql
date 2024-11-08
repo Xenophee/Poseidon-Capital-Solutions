db91e2de43536937c47472224f39018ba9316dc3
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-11-08 12:55:15 +0100

    Suppression du fichier inutile et ajout de l'info sur le mdp

--- resources/data.sql
+++ resources/data.sql
@@ -95,5 +95,7 @@ CREATE TABLE Users (
   PRIMARY KEY (Id)
 );
 
-insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN");
-insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER");
+
+insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$YPd5pSpiTemYu2PTV98f7eXbOzYX/1FP4N2k5LBpm2joTSwMTCSQu", "ADMIN");
+insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$YPd5pSpiTemYu2PTV98f7eXbOzYX/1FP4N2k5LBpm2joTSwMTCSQu", "USER");
+ -- > Mot de passe : Abc123@!
