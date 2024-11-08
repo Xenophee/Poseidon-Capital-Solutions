94fa842db285945ea3901609737e2ee75e434359
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-11-08 12:22:20 +0100

    Ajout d'un constructeur sans arguments

--- src/main/java/com/nnk/springboot/domain/UserEntity.java
+++ src/main/java/com/nnk/springboot/domain/UserEntity.java
@@ -3,10 +3,12 @@ package com.nnk.springboot.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Data
+@NoArgsConstructor
 @Table(name = "users")
 public class UserEntity {
 
@@ -26,4 +28,12 @@ public class UserEntity {
 
     @NotBlank(message = "Role is mandatory")
     private String role;
+
+
+    public UserEntity(String username, String password, String fullname, String role) {
+        this.username = username;
+        this.password = password;
+        this.fullname = fullname;
+        this.role = role;
+    }
 }
