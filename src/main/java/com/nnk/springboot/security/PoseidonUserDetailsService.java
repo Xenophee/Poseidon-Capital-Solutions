2f7dbd9558f29bc35678186cfe399dd8cb7b202e
Perrine Dassonville <perrine.dassonville@outlook.fr>
2024-11-08 12:55:25 +0100

    Ajout de la javadoc

--- src/main/java/com/nnk/springboot/security/PoseidonUserDetailsService.java
+++ src/main/java/com/nnk/springboot/security/PoseidonUserDetailsService.java
@@ -29,6 +29,19 @@ public class PoseidonUserDetailsService implements UserDetailsService {
     }
 
 
+    /**
+     * Charge les détails d'un utilisateur pour l'authentification en se basant sur son nom d'utilisateur.
+     * Cette méthode recherche l'utilisateur dans le référentiel {@link UserRepository} et,
+     * s'il est trouvé, convertit l'entité {@link UserEntity} en un utilisateur de sécurité
+     * compatible avec Spring Security via {@link UserDetails}.
+     *
+     * <p>Si aucun utilisateur correspondant n'est trouvé, une exception {@link UsernameNotFoundException}
+     * est levée, et un avertissement est enregistré dans les logs.</p>
+     *
+     * @param username le nom d'utilisateur de l'utilisateur à authentifier
+     * @return une instance de {@link UserDetails} contenant les informations de l'utilisateur pour l'authentification
+     * @throws UsernameNotFoundException si aucun utilisateur correspondant n'est trouvé dans le référentiel
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
@@ -41,6 +54,17 @@ public class PoseidonUserDetailsService implements UserDetailsService {
     }
 
 
+    /**
+     * Crée et retourne un utilisateur de sécurité Spring {@link User} basé sur un utilisateur de l'application
+     * représenté par un {@link UserEntity}. Cette méthode extrait le rôle de l'utilisateur, le convertit en
+     * autorité de sécurité, puis crée une instance de {@link User} pour les contrôles d'authentification et d'autorisation.
+     *
+     * <p>Le rôle de l'utilisateur est préfixé avec "ROLE_" pour se conformer aux conventions de Spring Security.
+     * Cette instance de {@link User} est ensuite utilisée par le framework pour gérer les sessions et les contrôles d'accès.</p>
+     *
+     * @param userEntity l'entité {@link UserEntity} représentant un utilisateur dans l'application
+     * @return une instance de {@link User} avec le nom d'utilisateur, le mot de passe et les autorités de sécurité
+     */
     private User createSecurityUser(UserEntity userEntity) {
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + userEntity.getRole());
         return new User(userEntity.getUsername(), userEntity.getPassword(), List.of(authority));
--- src/main/java/com/nnk/springboot/security/SecurityConfiguration.java
+++ src/main/java/com/nnk/springboot/security/SecurityConfiguration.java
@@ -30,6 +30,19 @@ public class SecurityConfiguration {
     }
 
 
+    /**
+     * Configure et initialise le bean {@link SecurityFilterChain} pour gérer la sécurité HTTP dans l'application.
+     * Cette méthode définit les règles d'autorisation pour différents endpoints, la configuration de la page
+     * de connexion et de déconnexion, ainsi que le traitement des exceptions.
+     *
+     * <p>La configuration spécifie des autorisations pour les ressources statiques et pour certains rôles.
+     * Elle définit également une page de connexion personnalisée, une redirection après succès, et une
+     * URL de déconnexion personnalisée qui invalide la session et supprime les cookies.</p>
+     *
+     * @param http l'instance de {@link HttpSecurity} pour la configuration de la sécurité HTTP
+     * @return une instance de {@link SecurityFilterChain} configurée pour gérer la sécurité HTTP
+     * @throws Exception si une erreur de configuration survient lors de la création de la chaîne de filtres
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -58,6 +71,19 @@ public class SecurityConfiguration {
     }
 
 
+    /**
+     * Configure et initialise le bean {@link AuthenticationManager} utilisé pour l'authentification des utilisateurs
+     * dans l'application. Cette méthode définit le service de gestion des détails d'utilisateur et le
+     * codeur de mot de passe.
+     *
+     * <p>Cette configuration utilise un {@link HttpSecurity} pour obtenir un {@link AuthenticationManagerBuilder}
+     * partagé, puis y enregistre le service d'authentification {@code poseidonUserDetailsService}
+     * ainsi que le codeur de mot de passe {@code passwordEncoder} pour les vérifications de mot de passe.</p>
+     *
+     * @param http l'instance de {@link HttpSecurity} partagée permettant d'accéder au gestionnaire d'authentification
+     * @return un {@link AuthenticationManager} configuré avec le service de détails utilisateur et le codeur de mot de passe
+     * @throws Exception si une erreur de configuration survient lors de la création du {@link AuthenticationManager}
+     */
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
 
